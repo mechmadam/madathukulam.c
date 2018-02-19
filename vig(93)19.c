1f5a14a099ed78edb9c9064372ea5f5c462d723e
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-02-19 16:01:58 +0530

    vig(93)19.c

---
+++ vig(93)19.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[10],i,n,j,temp;
+printf("Enter the elements in array");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",a[i]);
+}
+for(i=1;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+if(a[i]<a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+printf("sorted elements are:\t");
+	for(i=0;i<n;i++)
+	{
+  printf("%d\n \t",a[i]);	
+}
+}
