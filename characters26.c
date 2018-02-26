162b44b2205bb603814ecefc15144c88633ab0a6
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-02-26 16:10:05 +0530

    Create characters26.c

---
+++ characters26.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char strp[100];
+    int counter,count=0,countspace;
+    for(counter=0;str[counter]!='\0';counter++)
+	{
+	if((str[counter]>='0' && str[counter]<='9')
+	|| (str[counter]>='A' && str[counter]<='Z')
+	|| (str[counter]>='a' && str[counter]<='z')
+          || (str[counter]==' ' || str[counter]== '.'))
+            countSpace++;
+            else
+            counts++;
+            }
+            printf(" %d count");
+            getch();
+            return 0;
+            }
+   
