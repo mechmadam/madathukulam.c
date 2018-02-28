577e934acd8dd776cf6e79de6d483ff63fcca169
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-02-28 16:14:10 +0530

    Create stringK28.C

---
+++ stringK28.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char s[50];
+int k,i;
+printf("Enter the string:");
+scanf("%d",&s);
+printf("Enter the numner ");
+scanf("%d",&k);
+for(i=0;i<k;i++)
+{
+printf("%s \n",s);
+}
+getch();
+return 0;
+}
