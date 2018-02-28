07d7593a93b1d9bb1924aede25ed603c1b1012c1
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-02-28 15:57:54 +0530

    Create among28.c

---
+++ among28.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[10],i,large;
+for(i=0;i<10;i++)
+{
+scanf("%d",&a[i]);
+}
+a[0]=large;
+for(i=0;i<10;i++)
+{
+	if(a[i]>large)
+	{
+	large=a[i];
+	}
+  }
+  printf("%d",large);
+  getch();
+  return 0;
+  }
+  
