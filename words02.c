34f5ea0131f78266393af9f0100d880e5504df1f
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-03-02 14:01:27 +0530

    words02.c

---
+++ words02.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("\n enter the number ");
+scanf("%d",&n);
+switch(n)
+{
+case 1:printf("one");
+break;
+case 2:printf("two");
+break;
+}
+getch();
+return 0;
+}
