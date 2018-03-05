ce381640a3f491872dfc088cbf7673df456e837f
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-03-05 16:01:42 +0530

    Create evennum05.c

---
+++ evennum05.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int num,even;
+printf("Enter the number \n");
+scanf("%d",&num);
+if(num%2==0)
+{
+even=num;
+}
+else
+{
+even=num-1;
+}
+printf("\n %d",even);
+}
+printf("\n %d",even);
+}
