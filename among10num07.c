c38f54b129a58e99db78a9569d9152016eb3ac1a
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-03-07 15:53:22 +0530

    among10num07.c

---
+++ among10num07.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[10],i,large;
+for(j=0;j<10;j++)
+{
+scanf("%d",&a[j]);
+}
+a[0]=large;
+for(j=0;j<n;j++)
+{
+if(a[j]<large)
+{
+large=a[j];
+}
+}
+printf("%d",large);
+getch();
+return 0;
+}
+
