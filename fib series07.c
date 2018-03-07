8907aee45b435e735e5d45864fd93c5a1b35959c
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-03-07 15:59:33 +0530

    fib series07.c

---
+++ fib series07.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,first=0,second=1,next,i;
+	scanf("%d",&n);
+for(i=1;i<=a;i++)
+{
+	if(i<=1)
+	next=i;
+  else
+  {
+  next=first+second;
+		first=second;
+		second=next;
+    }
+    printf("%d",next);
+    getch();
+    return 0;
+    }
+    
