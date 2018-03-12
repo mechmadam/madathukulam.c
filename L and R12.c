4d667c3529b4d704785baa738cb1e983460db7e4
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-03-12 16:18:04 +0530

    Create L and R12.c

---
+++ L and R12.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int N,L,R;
+	scanf("%d %d %d",&N,&L,&R);
+	if(N>=L && N<=R)
+	{
+		printf("Yes");
+	}
+	else
+	{
+		printf("NO");
+	}
+  r
+  getch();
+  return 0;
+  }
+  
