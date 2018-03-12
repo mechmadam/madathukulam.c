4e2aa8137c80ff8cc3626ffab934a3111e2128fe
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-03-12 16:31:08 +0530

    Create middle12.c

---
+++ middle12.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,l;
+char a[5]="laptop";
+l=strlen(a);
+if(l%2==0)
+{
+a[l/2]='*';
+}
+else
+{
+a[(l-1)/2]='*';
+}
+for(i=0;a[i]!='\0';i++)
+{
+printf("the middle element is %c",a[i]);
+}
+getch();
+return 0;
+}
