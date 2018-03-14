3c41e72b28199897f8d24010ef8dd4612d2a3e5e
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-03-14 16:16:49 +0530

    Create find14.c

---
+++ find14.c
@@ -0,0 +1,19 @@
+#include<sdtio.h>
+#include<conio.h>
+int main()
+{
+int u;
+clrscr();
+printf("Enter a number:");
+scanf("%d",&u);
+if(s%13==0)
+{
+printf("YES");
+}
+else
+{
+printf("NO");
+}
+getch();
+return 0;
+}
