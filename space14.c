cbeb530b5984aaac46fe77e27e61e82121b9defb
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-03-14 16:25:15 +0530

    Create space14.c

---
+++ space14.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char a[50]="12345";
+int i,l;
+l=strlen(a);
+for(i=0;i<l;i++)
+{
+if(a[i]%2!=0)
+{
+printf("%c",a[i]);
+}
+}
+getch()
+return 0;
+}
