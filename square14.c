e1d8000fc1dd8f1d30827c5b2c791820121b754f
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-03-14 16:21:42 +0530

    Create square14.c

---
+++ square14.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+clrscr();
+scanf("%d%d",&a,&b);
+c=a*b;
+if(a==b)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+return 0;
+}
