0bfe58e69d591a7da183830e5233ec2fffdee087
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-03-16 16:00:10 +0530

    Create junga16.c

---
+++ junga16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int main(void) {
+	int a[10],b[10],c[10];
+	int j;
+	for(j=0;j<4;j++)
+	{
+		scanf("%d %d",&a[j],&b[j]);
+		if(j%2==0)
+		c[j]=a[j]/b[j];
+		else
+		c[j]=a[j]%b[j];
+	}
+  for(i=0;i<n;i++)
+  {
+  
+		printf("\n%d",c[j]);
+	}
+	return 0;
+  }
+  
